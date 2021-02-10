604be1908bd41ddc25265e1ca678f5ed9ce3f2df
naftalib <45719650+naftalib@users.noreply.github.com>
2021-02-10 02:39:21 +0200

    complete

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ const App = () => {
   const [url, setUrl] = useState(
         "http://hn.algolia.com/api/v1/search?query=react"
   )
-
-
   //fetch news
   const fetchNews = () => {
     fetch(url)
@@ -23,29 +21,28 @@ const App = () => {
   const handleChange = (e) =>{
     setSearchQuery(e.target.value)
   }
-
   const handleSubmit = (e) => {
     e.preventDefault()
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
   }
-
   const searchBar = () => (
     <form onSubmit={handleSubmit}>
       <input type="text" value={searchQuery} onChange={handleChange} />
       <button>Search</button>
     </form>
   )
-
   const showNews = () => news.map((n,i) => <p key={i}>{n.title}</p>)
   
   return(
     <div>
-      <h2>Hacker News Search</h2>
-      {searchBar()}
-      {showNews()}
+      <div id='title'>
+        <h2>Hacker News Search</h2>
+      </div>
+      <div id='main'>
+        {searchBar()}
+        {showNews()}
+      </div>
     </div>
-  )
-  
+  ) 
 }
-
 export default App;
