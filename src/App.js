26f66d80130e6485594700fa50048188eea8c7d8
naftalib <45719650+naftalib@users.noreply.github.com>
2021-02-14 14:44:53 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,48 +1,68 @@
-import React, { useState, useEffect } from "react"
+import React, { Component } from "react"
 
-const App = () => {
+class App extends Component {
   //state innit
-  const [news, setNews] = useState([])
-  const [searchQuery, setSearchQuery] = useState('react')
-  const [url, setUrl] = useState(
-        "http://hn.algolia.com/api/v1/search?query=react"
-  )
-  //fetch news
-  const fetchNews = () => {
-    fetch(url)
-    .then(result => result.json())
-    .then(data => setNews(data.hits))
-    .catch(error => console.log(error))
-  }
-  useEffect(() => {
-    fetchNews()
-  }, [url])
 
-  const handleChange = (e) =>{
-    setSearchQuery(e.target.value)
+  state = {
+      news:[],
+      // searchQuery:'react',
+      // url: "http://hn.algolia.com/api/v1/search?query=react"
   }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
-  }
-  const searchBar = () => (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={searchQuery} onChange={handleChange} />
-      <button>Search</button>
-    </form>
-  )
-  const showNews = () => news.map((n,i) => <p key={i}>{n.title}</p>)
+//   const [news, setNews] = useState([])
+//   const [searchQuery, setSearchQuery] = useState('react')
+//   const [url, setUrl] = useState(
+//         "http://hn.algolia.com/api/v1/search?query=react"
+//   )
+  //fetch news
+componentDidMount(){
+    fetch("http://hn.algolia.com/api/v1/search?query=php")
+    .then(result => result.json())
+    .then(data => {
+      console.log(data)
+        this.setState({
+            news: data.hits
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+//   const fetchNews = () => {
+//     fetch(url)
+//     .then(result => result.json())
+//     .then(data => setNews(data.hits))
+//     .catch(error => console.log(error))
+//   }
+//   useEffect(() => {
+//     fetchNews()
+//   }, [url])
+
+//   const handleChange = (e) =>{
+//     setSearchQuery(e.target.value)
+//   }
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+//   }
+//   const searchBar = () => (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" value={searchQuery} onChange={handleChange} />
+//       <button>Search</button>
+//     </form>
+//   )
   
+  render(){
+    const showNews = this.state.news.map((n,i) => <p key={i}>{n.title}</p>)
+
   return(
     <div>
       <div id='title'>
         <h2>Hacker News Search</h2>
       </div>
       <div id='main'>
-        {searchBar()}
-        {showNews()}
+       <h2>{showNews}</h2>
       </div>
     </div>
   ) 
+  }
 }
 export default App;
